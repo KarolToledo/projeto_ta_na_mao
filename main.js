cbe8389b556b90faeb3add1c89c99753786e7615
karoltoledo <karolinatoledo2010@gmail.com>
2022-06-19 15:00:08 -0300

    acrescimo de layout responsivo

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// References to DOM Elements
 const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
 const book = document.querySelector("#book");
@@ -7,11 +6,9 @@ const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
 
-// Event Listener
 prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
-// Business Logic
 let currentLocation = 1;
 let numOfPapers = 3;
 let maxLocation = numOfPapers + 1;
